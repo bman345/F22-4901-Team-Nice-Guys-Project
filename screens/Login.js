11367f85e0018409d857692bf15277deb05d678d
eeg0499 <108757801+eeg0499@users.noreply.github.com>
2022-11-02 19:34:33 -0500

    Updated Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -1,22 +1,140 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+
+} from "react-native";
+
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("../assets/BabyTrackerLogo2.png")} />
 
 
-export default function LoginScreen() {
-  
-    return (
-      <View style={styles.container}>
-        <Text>This is the Login Screen</Text>
-        <StatusBar style="auto" />
+     
+      <StatusBar style="auto" />
+
+      <View style={styles.inputView}>
+
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          placeholderTextColor="#000000"
+          onChangeText={(email) => setEmail(email)}
+
+        />
+
       </View>
-    );
-  }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#D2FFB7',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+      <View style={styles.inputView}>
+
+        <TextInput
+
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#000000"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+
+ 
+
+      <TouchableOpacity style={styles.registerBtn}>
+        <Text style={styles.registerText}>REGISTER</Text>
+      </TouchableOpacity>
+
+    </View>
+
+  );
+
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: "#D2FFB7",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image: {
+    height: 200,
+    width: 200,
+    marginBottom: 40,
+    marginLeft:40,
+  },
+
+/*
+  imageLetter: {
+    height: 200,
+    width: 200,
+    top: -100,
+  },
+*/
+
+  inputView: {
+    backgroundColor: "#7DC265",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+
+  TextInput: {
+    height: 50,
+   flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#82A16E",
+  },
+
+  registerBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#7DC265",
+  },
+
+ 
+
+});
