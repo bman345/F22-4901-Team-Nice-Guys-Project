f8961bb12420e2a84c0fef21fc2ab34db9b9afd0
Aracely Heredia <93110765+ah0961@users.noreply.github.com>
2023-01-26 18:19:30 -0600

    Create Account.js

---
+++ screens/Account.js
@@ -0,0 +1,215 @@
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Switch,
+} from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+
+const SECTIONS = [
+  {
+    header: 'Preferences',
+    icon: 'settings',
+    items: [
+      
+      { icon: 'user', color: '#32c759', label: 'Account Information', type: 'link' },
+      { icon: 'phone', color: '#fd2d54', label: 'Emergency Contact', type: 'link' },
+      { icon: 'users', color: '#fe9400', label: 'Manage Accounts', type: 'link' },
+      {
+        //id: 'darkMode',
+        icon: 'moon',
+        color: '#007afe',
+        label: 'Dark Mode',
+        //setForm: false,
+        type: 'toggle',
+      },
+     
+    ],
+  },
+  {
+    header: 'Help',
+    icon: 'help-circle',
+    items: [
+      { icon: 'flag', color: '#8e8d91', label: 'Report Bug', type: 'link' },
+      { icon: 'mail', color: '#007afe', label: 'Contact Us', type: 'link' },
+    ],
+  },
+
+];
+
+
+export default function Example() {
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.profile}>
+          <TouchableOpacity
+            onPress={() => {
+              // handle onPress
+            }}>
+            <View style={styles.profileAvatarWrapper}>
+              <Image
+                alt=""
+                source={require("../assets/BabyTrackerLogo.png")}
+                style={styles.profileAvatar}
+              />
+
+              <TouchableOpacity
+                onPress={() => {
+                  // handle onPress
+                }}>
+                <View style={styles.profileAction}>
+                  <FeatherIcon color="#fff" name="edit-3" size={15} />
+                </View>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+
+          <View style={styles.profileBody}>
+            <Text style={styles.profileName}> Guest </Text>
+
+            <Text style={styles.Role}>
+              Admin: Parent
+            </Text>
+          </View>
+        </View>
+
+        {SECTIONS.map(({ header, items }) => (
+          <View style={styles.section} key={header}>
+            <Text style={styles.sectionHeader}>{header}</Text>
+            {items.map(({ label, icon, type, value, color }, index) => {
+              return (
+                <TouchableOpacity
+                  key={label}
+                  onPress={() => {
+                    // handle onPress
+                  }}>
+                  <View style={styles.row}>
+                    <View style={[styles.rowIcon, { backgroundColor: color }]}>
+                      <FeatherIcon color="#fff" name={icon} size={18} />
+                    </View>
+
+                    <Text style={styles.rowLabel}>{label}</Text>
+
+                    <View style={styles.rowSpacer} />
+
+                    {type === 'toggle' && (
+                    <Switch 
+                    value={isEnabled} 
+                    onValueChange={toggleSwitch}
+                    />
+                    )}
+
+                    {type === 'link' && (
+                      <FeatherIcon
+                        color="#0c0c0c"
+                        name="chevron-right"
+                        size={22}
+                      />
+                    )}
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 24,
+  },
+  section: {
+    paddingHorizontal: 24,
+  },
+  sectionHeader: {
+    paddingVertical: 12,
+    fontSize: 12,
+    fontWeight: '600',
+    color: '#9e9e9e',
+    textTransform: 'uppercase',
+    letterSpacing: 1.1,
+  },
+  profile: {
+    padding: 24,
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profileAvatar: {
+    width: 90,
+    height: 90,
+    borderRadius: 9999,
+  },
+  profileAvatarWrapper: {
+    position: 'relative',
+  },
+  profileAction: {
+    position: 'absolute',
+    right: -4,
+    bottom: -10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 28,
+    height: 28,
+    borderRadius: 9999,
+    backgroundColor: '#007bff',
+  },
+  profileName: {
+    marginTop: 20,
+    fontSize: 19,
+    fontWeight: '600',
+    color: '#414d63',
+    textAlign: 'center',
+  },
+  profileRole: {
+    marginTop: 5,
+    fontSize: 16,
+    color: '#989898',
+    textAlign: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: 50,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 8,
+    marginBottom: 12,
+    paddingLeft: 12,
+    paddingRight: 12,
+  },
+  rowIcon: {
+    width: 32,
+    height: 32,
+    borderRadius: 9999,
+    marginRight: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rowLabel: {
+    fontSize: 17,
+    fontWeight: '400',
+    color: '#0c0c0c',
+  },
+  rowSpacer: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+});
