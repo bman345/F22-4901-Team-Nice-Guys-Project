169ec46641549158b84145178b2efadaedbbad23
Aracely <93110765+ah0961@users.noreply.github.com>
2023-02-13 23:52:35 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,126 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { NavigationContainer, DarkTheme, DefaultTheme } from '@react-navigation/native';
+//import * as React from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import themeContext from './config/themeContext';
+import theme from './config/theme';
+import { EventRegister } from 'react-native-event-listeners';
+import React, {useState, useEffect} from 'react';
+
+import HomeScreen from './screens/HomeScreen';
+import RegistrationScreen from './screens/Registration';
+import LoginScreen from './screens/Login';
+import CreateBaby from './screens/CreateBaby';
+import Calendar from './screens/CalendarScreen';
+import Account from './screens/Account';
+import Reminders from './screens/Reminders';
+import Track from './screens/Track';
+import Analysis from './screens/Analysis';
+
+//stack variable for page navigation in app
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function CalendarTab(){
+  return(
+   
+      <Tab.Navigator screenOptions={{
+        tabBarActiveTintColor: '#7DC265',
+        tabBarLabelStyle:{
+          fontSize: 15,
+        },
+
+        tabBarStyle:{
+          flex: 1,
+          position: 'absolute',
+          botttom: 25,
+          elevation: 0,
+          background: {backgroundColor: theme.background},
+          height: 90,
+        }, 
+      }}>
+    
+
+        <Tab.Screen name="Reminders" component={Reminders} options={
+          {tabBarIcon: ({ color, size })=> 
+          (<MaterialCommunityIcons name="calendar-clock" size={size} color={color} />)}}
+          />
+
+        <Tab.Screen name="Calendars" component={Calendar} options={
+          {tabBarIcon: ({ color, size }) => 
+          (<Ionicons name='calendar-outline' color={color} size={size}/>)}}
+        />
+
+        <Tab.Screen name="Track" component={Track} options={
+          {tabBarIcon: ({ color, size }) => 
+          (<Ionicons name='add-circle' color={color} size={size}/>)}} 
+        />
+
+        <Tab.Screen name="Analysis" component={Analysis} options={
+          {tabBarIcon: ({ color, size }) => 
+          (<Ionicons name='bar-chart' color={color} size={size}/>)}}
+        />
+   
+        <Tab.Screen name="Account" component={Account}  options={
+          {tabBarIcon: ({ color, size })=> 
+          (<MaterialCommunityIcons name="account" size={size} color={color} />)}}
+        />
+     
+
+      </Tab.Navigator>
+      
+  );
+}
+
+
+
+export default function App() {
+  const [mode, setMode] = useState(false);
+
+useEffect(() => {
+
+  let eventListener = EventRegister.addEventListener("changeTheme", (data) => {
+    setMode(data);
+    //console.log(data);
+  }
+  );
+return () => {
+
+  EventRegister.removeEventListener(eventListener);
+};
+}
+);
+
+  return (
+    <themeContext.Provider value={mode === true ? theme.dark : theme.light}>
+    <NavigationContainer theme={mode === true ? DarkTheme : DefaultTheme}>
+ 
+      <Stack.Navigator>
+
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Create Baby" component={CreateBaby} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Registration" component={RegistrationScreen} />
+        <Stack.Screen name="Calendar" component={CalendarTab} options={{ headerShown: false }} />
+       
+      </Stack.Navigator>
+
+    </NavigationContainer>
+    </themeContext.Provider>
+  );
+}
+
+
+
+/* the bottom stuff was what originally was here but I just commented it out so that you guys can see
+app.js working with the rest of the pages (w/o firebase)
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////
+/*
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "./screens/HomeScreen";
@@ -52,6 +175,7 @@ export default function App() {
       </NavigationContainer>
     );
   }
+*/
 
 /* This is breaking the navigation container for some reason, so 
    we'll fix this later
@@ -75,6 +199,6 @@ export default function App() {
     </NavigationContainer>
   );
     */
-}
+//}
 
 
