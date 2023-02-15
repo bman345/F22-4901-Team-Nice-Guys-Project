fc307fa2ab2e9eee51ff51af46b1e2082b77a0c7
Aracely <93110765+ah0961@users.noreply.github.com>
2023-02-15 13:42:58 -0600

    Create theme.js

---
+++ config/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+
+light: {
+    theme:"light",
+    color: "black",
+    background: "white",
+},
+dark: {
+    theme:"dark",
+    color: "white",
+    background: "black",
+},
+
+
+};
+
+export default theme;
