020f119ed29a11dee46d01814ba34e523bda191a
Saul <aaaambp@gmail.com>
2023-04-24 17:38:45 -0500

    Feeding analysis

--- screens/Analysis.js
+++ screens/Analysis.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, View } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import React, { useState } from 'react';
+import FeedingAnalysis from './FeedingAnalysis';
+
+const SubScreens = [
+  { label: 'Feedings', value: 'feeding-analysis' },
+  { label: "placeholder", value: 'placeholder' }
+];
 
 export default function Analysis() {
+  const [selectedScreen, setSelectedScreen] = useState('feeding-analysis');
+
+  const renderScreen = () => {
+    switch (selectedScreen) {
+      case 'feeding-analysis':
+        return <FeedingAnalysis />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <Text>My name is Guest</Text>
-      <StatusBar style="auto" />
+      <Dropdown
+        label="Select a screen"
+        data={SubScreens}
+
+        style={styles.dropdown}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder={"Select Analysis"}
+        onChange={item => {
+          setSelectedScreen(item.value);
+        }}
+      />
+      {renderScreen()}
     </View>
   );
 }
@@ -14,8 +46,42 @@ export default function Analysis() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 50,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  container: {
+    flex: 1,
     backgroundColor: '#4628',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dropdown: {
+    height: 50,
+    width: '80%',
+    borderColor: 'gray',
+    backgroundColor: 'white',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
 });
+
---
+++ screens/FeedingAnalysis.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
+
+import moment from 'moment';
+
+const FeedingAnalysis = () => {
+
+  const feedData = [
+    { time: '2022-04-24T01:00:00.000Z', date: '2023-04-24' },
+    { time: '2022-04-24T02:30:00.000Z', date: '2023-04-24' },
+    { time: '2022-04-24T04:45:00.000Z', date: '2023-04-24' },
+    { time: '2022-04-24T06:15:00.000Z', date: '2023-04-24' },
+    { time: '2022-04-24T20:00:00.000Z', date: '2023-04-24' },
+    { time: '2022-04-24T22:30:00.000Z', date: '2023-04-24' },
+    { time: '2022-04-25T01:15:00.000Z', date: '2022-04-25' },
+    { time: '2022-04-25T03:30:00.000Z', date: '2022-04-25' },
+    { time: '2022-04-25T06:00:00.000Z', date: '2022-04-25' },
+    { time: '2022-04-25T08:45:00.000Z', date: '2022-04-25' },
+    { time: '2022-04-25T11:15:00.000Z', date: '2022-04-25' },
+    { time: '2022-04-25T13:30:00.000Z', date: '2022-04-25' },
+    { time: '2022-04-25T16:00:00.000Z', date: '2022-04-25' },
+    { time: '2022-04-25T18:30:00.000Z', date: '2022-04-25' },
+    { time: '2022-04-25T21:00:00.000Z', date: '2022-04-25' },
+    { time: '2022-04-25T23:15:00.000Z', date: '2022-04-25' }
+  ];
+
+  const getChartData = (feedData) => {
+    // Create an array with empty objects for each day of the week
+    const chartData = [...Array(7)].map(() => ({ date: '', count: 0 }));
+
+    // Loop through the feedData array and increment the count for the appropriate day
+    feedData.forEach((feed) => {
+      const dayOfWeek = new Date(feed.time).getDay();
+
+      chartData[dayOfWeek].date = moment(feed.time).format('MMM DD');
+      chartData[dayOfWeek].count++;
+    });
+
+    return chartData;
+  };
+
+  const data = {
+    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    datasets: [
+      {
+        data: getChartData(feedData).map((item) => item.count),
+        color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`, // blue color for bars
+      },
+    ],
+  };
+
+  const formatYLabel = (value) => {
+    return ""
+  };
+
+  const chartConfig = {
+    backgroundGradientFrom: "#fff",
+    backgroundGradientTo: "#fff",
+    decimalPlaces: 0,
+    color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
+    labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+  };
+
+  return (
+      <BarChart
+        data={data}
+        width={400}
+        height={220}
+        yAxisSuffix=' feeds'
+        chartConfig={chartConfig}
+        style={{ marginVertical: 8 }}
+      />
+  )
+};
+
+export default FeedingAnalysis;
+
+
+
+
