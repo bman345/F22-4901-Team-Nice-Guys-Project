77a968fadb211ad22e4a9419de2857e4897429f8
Eduardo Garcia <108757801+eeg0499@users.noreply.github.com>
2023-04-27 00:05:46 -0500

    Updated Track.js

--- screens/Track.js
+++ screens/Track.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Pressable, Image } from 'react-native';
 import { getFirebaseAuth, getReference } from '../Firebase';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Icon, SafeAreaView, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
+export default function TrackScreen({ navigation }) {
 
-export default function Track() {
+const [currentDate, setcurrentDate] = useState('');
+
+ 
+
+useEffect(() => {
+const date = new Date().getDate(); // current date
+const month = new Date().getMonth() + 1; // current month
+const year = new Date().getFullYear(); // current year
+setcurrentDate(`${month}/${date}/${year}`);
+}, []);
 
   const auth = getFirebaseAuth();
+  
   return (
     <View style={styles.container}>
       <Pressable style={styles.Button2} onPress={() => auth.signOut()}>
@@ -13,31 +28,189 @@ export default function Track() {
       </Pressable>
       <StatusBar style="auto" />
     </View>
+      <Text style={styles.title}>Today's Date</Text>
+      <View style={[styles.currentDateContainer, {top: -8}]}>
+        <Text style={styles.currentDate}>{currentDate}</Text>
+        <View style={styles.separator} />
+      </View>
+      <TouchableOpacity onPress={() => navigation.navigate("Feedings")}>
+        <View style={styles.feedingContainer}>
+          <Text style={styles.feedingText}>Feeding</Text>
+        </View>
+      </TouchableOpacity>
+
+ 
+      <TouchableOpacity onPress={() => navigation.navigate('Diapers')}>
+        <View style={styles.diaperContainer}>
+          <Text style={styles.diaperText}>Diaper</Text>
+         </View>
+      </TouchableOpacity>
+
+ 
+
+      <TouchableOpacity onPress={() => navigation.navigate('Sleeps')}>
+        <View style={styles.sleepContainer}>
+          <Text style={styles.sleepText}>Sleep</Text>
+         </View>
+      </TouchableOpacity>
+ 
+
+      <TouchableOpacity onPress={() => navigation.navigate('Doc')}>
+        <View style={styles.doctorContainer}>
+          <Text style={styles.doctorText}>Doctor Vists</Text>
+         </View>
+      </TouchableOpacity>
+
+ 
+
+      <TouchableOpacity onPress={() => navigation.navigate('Meds')}>
+        <View style={styles.medicationContainer}>
+          <Text style={styles.medicationText}>Medication</Text>
+         </View>
+      </TouchableOpacity>
+ 
+      <StatusBar style="auto" />
+
+    </View>
   );
 }
 
+ 
+
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#4628',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
-  Button2: {
-    padding: 10,
-    backgroundColor: '#82A16E',
-    width: 294,
-    height: 64,
-    borderRadius: 6,
-    marginBottom: 25,
-    marginTop: 25,
-},
+  background: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
 
-  Text: {
-    fontSize: 35,
+  topBackground: {
+    flex: 1,
+  },
+
+  bottomBackground: {
+    flex: 1,
+    backgroundColor: '#F6F6F6',
+  },
+
+  title: {
+    position: 'absolute',
+    fontSize: 20,
+    top: 40,
+  },
+
+  currentDateContainer: {
+    position: 'absolute',
+    alignItems: 'center',
+    marginTop: 75,
+    borderBottomWidth: 1,
+    borderBottomColor: '#333',
+  },
+
+  currentDate: {
+    fontSize: 16,
+    color: '#333',
+  },
+
+  separator: {
+    width:  300,
+    height: 1,
+    backgroundColor: '#333',
+    marginTop: 5,
+  },
+
+  icon: {
+    right: -142,
+    top: -240,
+  },
+
+  feedingContainer: {
+    position: 'absolute',
+    backgroundColor: '#E2CBFF',
+    padding: 10,
+    width: 300,
+    borderRadius: 5,
+    marginTop: -250,
+    right: -150,
+  },
+
+  feedingText: {
+    color: 'white',
+    fontWeight: 'bold',
     textAlign: 'center',
-    textAlignVertical: 'center',
+  },
+
+  diaperContainer: {
+    position: 'absolute',
+    backgroundColor: '#C7EDFF',
+    padding: 10,
+    width: 300,
+    borderRadius: 5,
+    marginTop: -190,
+    right: -150,
+  },
+
+  diaperText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
+  sleepContainer: {
+    position: 'absolute',
+    backgroundColor: '#BAEBD9',
+    padding: 10,
+    width: 300,
+    borderRadius: 5,
+    marginTop: -130,
+    right: -150,
+  },
+
+  sleepText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
+  doctorContainer: {
+    position: 'absolute',
+    backgroundColor: '#FFBABA',
+    padding: 10,
+    width: 300,
+    borderRadius: 5,
+    marginTop:-70,
+    right: -150,
+  },
+
+  doctorText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
+  medicationContainer: {
+    position: 'absolute',
+    backgroundColor: '#7DC265',
+    padding: 10,
+    width: 300,
+    borderRadius: 5,
+    marginTop: -10,
+    right: -150,
+  },
+
+  medicationText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
  
